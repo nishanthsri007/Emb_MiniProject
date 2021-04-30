ad77c0a01cb09789332acc9d360c776038981f06
Nishanth256555 <56690808+nishanthsri007@users.noreply.github.com>
2021-04-30 17:30:08 +0530

    Update Led.c

--- src/Led.c
+++ src/Led.c
@@ -7,16 +7,16 @@ volatile unsigned Switch_Status = 0;
 void Led_Actuator(void)
 {
     sei();
-    DDRB |= (1 << PB0); ///<Set B0 bit in DDRB register(to make it as output)
+    DDRB |= (1 << PB0);                      ///<Set B0 bit in DDRB register(to make it as output)
 
-    DDRD &= ~(1 << PD0); ///<Clear D0 bit in DDRD register
-    DDRC &= ~(1 << PC5); ///<Clear C5 bit in DDRD register
-    PORTD |= (1 << PD0); ///<Set D0 bit in PORTD register
-    PORTC |= (1 << PC5); ///<Set C5 bit in PORTD register
+    DDRD &= ~(1 << PD0);                     ///<Clear D0 bit in DDRD register
+    DDRC &= ~(1 << PC5);                     ///<Clear C5 bit in DDRD register
+    PORTD |= (1 << PD0);                     ///<Set D0 bit in PORTD register
+    PORTC |= (1 << PC5);                     ///<Set C5 bit in PORTD register
 
-    PCICR |= (1 << PCIE2) | (1 << PCIE1);  ///<Enable Pin Change Interrupt for port C and port D
-    PCMSK1 |= (1 << PCINT13);   ///<Set pin change interrupt for PC5
-    PCMSK2 |= (1 << PCINT16);   ///<Set pin change interrupt for PD0
+    PCICR |= (1 << PCIE2) | (1 << PCIE1);    ///<Enable Pin Change Interrupt for port C and port D
+    PCMSK1 |= (1 << PCINT13);                ///<Set pin change interrupt for PC5
+    PCMSK2 |= (1 << PCINT16);                ///<Set pin change interrupt for PD0
 
 }
 
@@ -25,9 +25,9 @@ ISR(PCINT1_vect)
 {
     ///Check if PC5 is high
     if(!(PINC & (1 << PINC5)))
-        Seat_Status = 1;  ///<Set variable
+        Seat_Status = 1;                     ///<Set variable
     else
-        Seat_Status = 0;  ///<Clear variable
+        Seat_Status = 0;                     ///<Clear variable
 }
 
 ///ISR for state change in pin D0
@@ -35,7 +35,7 @@ ISR(PCINT2_vect)
 {
     ///Check if PD0 is high
     if(!(PIND & (1 << PIND0)))
-        Switch_Status = 1;  ///<Set variable
+        Switch_Status = 1;                   ///<Set variable
     else
-        Switch_Status = 0;   ///<Clear variable
+        Switch_Status = 0;                   ///<Clear variable
 }
