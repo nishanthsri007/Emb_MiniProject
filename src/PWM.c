04dd3e85dd2d2baf54bca1378ae0dc3b7b45a836
Nishanth256555 <56690808+nishanthsri007@users.noreply.github.com>
2021-04-30 20:24:57 +0530

    Update PWM.c

--- src/PWM.c
+++ src/PWM.c
@@ -1,7 +1,7 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-uint8_t PWM_output(uint16_t ADC_value)
+uint8_t PWM(uint16_t adc_value)
 {
     DDRB |= (1 << PB1);  ///<Set PB1 bit in DDRB to make it as output pin
 
@@ -10,25 +10,25 @@ uint8_t PWM_output(uint16_t ADC_value)
     TCCR1B |= (1 << CS11) | (1 << CS10);
     uint8_t temp = 0;
 
-    if(ADC_value >= 0 && ADC_value <= 200)
+    if(adc_value >= 0 && adc_value <= 200)
     {
         OCR1A = 204;   ///<20% dutycycle
         temp = 20;
         _delay_ms(200);
     }
-    else if(ADC_value >= 210 && ADC_value <= 500)
+    else if(adc_value >= 210 && adc_value <= 500)
     {
         OCR1A = 410;   ///<40% dutycycle
         temp = 25;
         _delay_ms(200);
     }
-    else if(ADC_value >= 510 && ADC_value <= 700)
+    else if(adc_value >= 510 && adc_value <= 700)
     {
         OCR1A = 717;   ///<70% dutycycle
         temp = 29;
         _delay_ms(200);
     }
-    else if(ADC_value >= 710 && ADC_value <= 1024)
+    else if(adc_value >= 710 && adc_value <= 1024)
     {
         OCR1A = 973;   ///<95% dutycycle
         temp = 33;
