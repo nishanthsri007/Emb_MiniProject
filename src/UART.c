32ba1bbf1259656b64c89121afb8c81ef0fff496
Nishanth256555 <56690808+nishanthsri007@users.noreply.github.com>
2021-04-30 21:58:49 +0530

    Update UART.c

--- src/UART.c
+++ src/UART.c
@@ -9,10 +9,9 @@ void USARTInit(void)
     UBRR0H |= (BAUDRATE >> 8);
     UBRR0L |= BAUDRATE;
 
-    ///To set character size as 8 bits
+    ///Set character size to 8 bits
     UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
 
-    ///Enable transmit and receive interrupts
     UCSR0B = (1 << RXEN0) | (1 << TXEN0) | (1 << RXCIE0) | (1 << TXCIE0);
 }
 
@@ -24,7 +23,7 @@ char USARTReadChar(void)
     return UDR0;
 }
 
-void USARTWriteChar(char data)
+void USARTWriteChar(char value)
 {
     ///Wait to transmitt data
     while(!(UCSR0A & (1 << UDRE0)));
