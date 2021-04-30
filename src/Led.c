2b91b7eeb0a4a953a0b3380cd748111cb851fa8c
Nishanth256555 <56690808+nishanthsri007@users.noreply.github.com>
2021-04-30 11:06:06 +0530

    Update Led.c

--- src/Led.c
+++ src/Led.c
@@ -9,14 +9,14 @@ void ports_init(void)
     sei();
     DDRB |= (1 << PB0); ///<Set B0 bit in DDRB register(to make it as output)
 
-    DDRD &= ~(1 << PD0); ///<Clear D0 bit in DDRD register(to make it as input)
-    DDRC &= ~(1 << PC6); ///<Clear C6 bit in DDRD register(to make it as input)
-    PORTD |= (1 << PD0); ///<Set D0 bit in PORTD register(initial value 1)
-    PORTC |= (1 << PC6); ///<Set C6 bit in PORTD register(initial value 1)
+    DDRD &= ~(1 << PD0); ///<Clear D0 bit in DDRD register
+    DDRC &= ~(1 << PC5); ///<Clear C5 bit in DDRD register
+    PORTD |= (1 << PD0); ///<Set D0 bit in PORTD register
+    PORTC |= (1 << PC5); ///<Set C5 bit in PORTD register
 
     PCICR |= (1 << PCIE2) | (1 << PCIE1);  ///<Enable Pin Change Interrupt for port C and port D
-    PCMSK2 |= (1 << PCINT16);   ///<Set pin change interrupt for PC6
-    PCMSK1 |= (1 << PCINT14);   ///<Set pin change interrupt for PD0
+    PCMSK1 |= (1 << PCINT13);   ///<Set pin change interrupt for PC5
+    PCMSK2 |= (1 << PCINT16);   ///<Set pin change interrupt for PD0
 
 }
 
@@ -24,7 +24,7 @@ void ports_init(void)
 ISR(PCINT1_vect)
 {
     ///Check if PC6 is high
-    if(!(PINC & (1 << PINC6)))
+    if(!(PINC & (1 << PINC5)))
         Seat_Status = 1;  ///<Set variable
     else
         Seat_Status = 0;  ///<Clear variable
