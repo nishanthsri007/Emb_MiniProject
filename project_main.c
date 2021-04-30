f1e15bf508167f657e2837bdf1540884be8eacf0
Nishanth256555 <56690808+nishanthsri007@users.noreply.github.com>
2021-04-30 20:19:11 +0530

    Update project_main.c

--- project_main.c
+++ project_main.c
@@ -1,6 +1,5 @@
 /*
- */
-//#define F_CPU 16000000UL 
+ */ 
 #include <avr/io.h>
 #include <util/delay.h>
 #include "ADC.h"
@@ -13,13 +12,13 @@ int main()
 	uint16_t temp = 0;
 	int LED = 0;
 
-    ///Calling initializing functions
+    ///Call initializing functions
 	InitADC();
         Led_Actuator();
 
     while(1)
     {
-        ///Check for switch statuses and turn on/off the LED
+        ///Check for switch status and turn on/off the LED
 	    if(Seat_Status == 1 && Switch_Status == 1)
         {
             PORTB |= (1 << PB0);
@@ -34,12 +33,12 @@ int main()
         if(LED == 1)
         {
             temp = PWM_output(Read_ADC(0));  ///<Read ADC input and generate appropriate PWM output
-            USARTWriteChar(temp);  ///<Write temperature value to serial monitor
+            USARTWriteChar(temp);            ///<Write temperature value to serial monitor
             _delay_ms(200);
         }
         else
         {
-            PWM_output(2000);   ///<Function call to produce 0 PWM output
+            PWM_output(2000);               ///<Function call to produce 0 PWM output
         }
     }
     return 0;
